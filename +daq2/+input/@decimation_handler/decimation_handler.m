4d068fd4db0d02a062250d7915eacc7a6fda45e1
Jim Hokanson <jim.hokanson@gmail.com>
2018-01-15 18:01:39 -0500

    moved some decimation code to mex

--- +daq2/+input/@decimation_handler/decimation_handler.m
+++ +daq2/+input/@decimation_handler/decimation_handler.m
@@ -4,46 +4,8 @@ classdef decimation_handler < handle
     %   daq2.input.decimation_handler
     
     %{
-        
-        decimation_rates = [1 10 100]
-        
-        N = 1e6;
-        d1 = (1:N)';
-        data = [d1,d1,d1];
-    
-        profile on
-        tic
-        for k = 1:100
-            d = daq2.input.decimation_handler(decimation_rates);
-            I = unique(round((rand(1,5))*N));
-
-            if I(1) == 1
-                I(1) = 2;
-            end
-
-            I(end) = N;
-
-
-            dec_data1 = cell(1,3);
-            end_I = 0;
-            for i = 1:length(I)
-                start_I = end_I + 1;
-                end_I = I(i);
-                temp = d.getDecimatedData(data(start_I:end_I,:));    
-                for j = 1:3
-                    dec_data1{j} = vertcat(dec_data1{j},temp{j});
-                end
-            end
-
-            dec_data2 = d.getDecimatedData(data); 
-
-            if ~isequal(dec_data1,dec_data2)
-                error('Not equal')
-            end
-        end
-        toc
-        profile off
-        profile viewer
+        daq2.input.decimation_handler.runTest1
+        daq2.input.decimation_handler.runTest2
     
     %}
     
@@ -64,24 +26,120 @@ classdef decimation_handler < handle
         %State
         %--------------
         partial_buffers
+        buffers
         n_partial
         
         n_chans
+    end
+    
+    methods (Static)
+        function runTest1()
+            decimation_rates = [1 10 100];
+            
+            N = 1e6;
+            d1 = (1:N)';
+            data = [d1,d1,d1];
+            
+            profile on
+            tic
+            for k = 1:100
+                d = daq2.input.decimation_handler(10000,decimation_rates);
+                I = unique(round((rand(1,5))*N));
+                
+                if I(1) == 1
+                    I(1) = 2;
+                end
+                
+                I(end) = N;
+                
+                
+                dec_data1 = cell(1,3);
+                end_I = 0;
+                for i = 1:length(I)
+                    start_I = end_I + 1;
+                    end_I = I(i);
+                    temp = d.getDecimatedData(data(start_I:end_I,:));
+                    for j = 1:3
+                        dec_data1{j} = vertcat(dec_data1{j},temp{j});
+                    end
+                end
+                
+                dec_data2 = d.getDecimatedData(data);
+                
+                if ~isequal(dec_data1,dec_data2)
+                    error('Not equal')
+                end
+            end
+            toc
+            profile off
+            profile viewer
+        end
+     	function runTest2()
+            decimation_rates = [1 10 100];
+            
+            N = 1e6;
+            d1 = (1:N)';
+            data = [d1,d1,d1];
+            
+            profile on
+            tic
+            for k = 1:100
+                d = daq2.input.decimation_handler(10000,decimation_rates);
+                I = 10000:10000:N;
+                
+                I(end) = N;
+                
+                
+                dec_data1 = cell(1,3);
+                end_I = 0;
+                for i = 1:length(I)
+                    start_I = end_I + 1;
+                    end_I = I(i);
+                    temp = d.getDecimatedData(data(start_I:end_I,:));
+                    for j = 1:3
+                        dec_data1{j} = vertcat(dec_data1{j},temp{j});
+                    end
+                end
+                
+                dec_data2 = d.getDecimatedData(data);
+                
+                if ~isequal(dec_data1,dec_data2)
+                    error('Not equal')
+                end
+            end
+            toc
+            profile off
+            profile viewer
+        end
     end
     
     methods
         function obj = decimation_handler(samples_per_read,decimation_rates)
             %
             %   obj = daq2.input.decimation_handler(decimation_rates);
-                        
+            
+            
+            obj.samples_per_read = samples_per_read;
             obj.decimation_rates = decimation_rates;
             
             obj.n_chans = length(obj.decimation_rates);
             
+            %Setup buffers to store partial samples
+            %------------------------------------------------
+            %- These buffers store original samples that are not numerous
+            %enough to form one output sample
+            %- The max size is the decimation rate because one we have
+            %that # of input samples, we get one output sample
             obj.n_partial = zeros(1,obj.n_chans);
             obj.partial_buffers = cell(1,obj.n_chans);
             for i = 1:obj.n_chans
                 obj.partial_buffers{i} = zeros(1,obj.decimation_rates(i));
+            end
+            
+            %Setup buffers to store typical
+            obj.buffers = cell(1,obj.n_chans);
+            for i = 1:obj.n_chans
+                
             end
         end
         
@@ -93,17 +151,9 @@ classdef decimation_handler < handle
             %   Inputs
             %   ------
             %   input_data : [samples x channels]
-            %   
-                        
-
-            %TODO: This currently fails if we aren't reading in complete
-            %values ...
-            %i.e. if we are sampling at 1 Hz, and the daq rate is 1000
-            %and we read new data every 0.2 seconds, we need to wait until
-            %we have 1 seconds worth of data (5 reads) before
-            %we can output 1 sample.
+            %
             
-            dec_data = cell(1,obj.n_chans);
+            
             dec_rates = obj.decimation_rates;
             I = obj.n_partial;
             
@@ -111,7 +161,7 @@ classdef decimation_handler < handle
             for i = 1:obj.n_chans
                 n_per_sample = dec_rates(i);
                 if n_per_sample == 1
-                    dec_data{i} = input_data(:,i);
+                    obj.buffers{i} = input_data(:,i);
                     continue
                 end
                 
@@ -138,7 +188,7 @@ classdef decimation_handler < handle
                         
                         start_I = n_grab + 1;
                     else
-                        %We don't have enough for a full sample. Store the 
+                        %We don't have enough for a full sample. Store the
                         %data and continue
                         obj.partial_buffers{i}(n_old+1:n_old+n_grab) = input_data(1:n_grab,i);
                         continue
@@ -149,33 +199,38 @@ classdef decimation_handler < handle
                 
                 next_I = start_I + n_full_samples*n_per_sample;
                 
-                temp_data = input_data(start_I:next_I-1,i);
-                temp_data = reshape(temp_data,[n_per_sample n_full_samples]);
+                %New mex-based appproach
+                %----------------------------------------
+                obj.buffers{i} = columnPartialMean(input_data,i,start_I,n_per_sample,~isempty(first_sample));
                 
-                if isempty(first_sample)
-                    dec_data{i} = mean(temp_data,1)';
-                else
-                    dec_data{i} = [first_sample; mean(temp_data,1)'];
+                if ~isempty(first_sample)
+                    obj.buffers{i}(1) = first_sample;
                 end
-                            
+                
+               %Old non-mex based approach
+               %----------------------------------------------
+%              temp_data = input_data(start_I:next_I-1,i);
+%              temp_data = reshape(temp_data,[n_per_sample n_full_samples]);
+%              temp_data = mean(temp_data,1)';
+                
+%                 if isempty(first_sample)
+%                     dec_data{i} = temp_data;
+%                 else
+%                     dec_data{i} = [first_sample; temp_data];
+%                 end
+                
                 if next_I <= n_new
                     %Then we have extras to store ...
                     n_extra = n_new-next_I + 1;
-                    obj.buffers{i}(1:n_extra) = input_data(next_I:end,i);
+                    obj.partial_buffers{i}(1:n_extra) = input_data(next_I:end,i);
                     I(i) = n_extra;
                 end
-                
-%                 
-%                 
-%                 n_samples_out = n_new/n_per_sample;
-%                 temp_data = reshape(input_data(:,i),[n_per_sample n_samples_out]);
-%                 
-%                 %Store as column vectors
-%                 dec_data{i} = mean(temp_data,1)';
             end
             
             obj.n_partial = I;
-
+            
+            dec_data = obj.buffers;
+            
         end
     end
 end
